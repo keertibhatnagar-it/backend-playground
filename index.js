944b0356307e5ef88c1fd3f93786e35b25654d7c
Keerti Bhatnagar <kbhatnagar170@gmail.com>
2026-07-28 01:19:58 +0530

    feat: Add githubProfile object and endpoint to return user data

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const githubProfile = {
+  login: "keertibhatnagar",
+  id: 12345678,
+  avatar_url: "https://avatars.githubusercontent.com/u/12345678?v=4",
+  html_url: "https://github.com/keertibhatnagar",
+  name: "Keerti Bhatnagar",
+  bio: "Frontend Engineer | React | TypeScript | Next.js",
+  company: "Digitem Technologies",
+  location: "Gurugram, India",
+  blog: "https://keerti-bhatnagar.vercel.app",
+  twitter_username: null,
+  public_repos: 42,
+  public_gists: 3,
+  followers: 185,
+  following: 67,
+  created_at: "2021-12-10T09:15:30Z",
+  updated_at: "2026-07-27T10:20:45Z"
+};
 
+console.log(githubProfile.name); // Keerti Bhatnagar
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -12,7 +31,9 @@ app.get('/login', (req, res) => {
 app.get('/html-login', (req, res) => {
   res.send('<h1>you are on the login screen!</h1>');
 });
-
+app.get('/github', (req, res) => {
+    res.json(githubProfile);
+});
 app.listen( process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
